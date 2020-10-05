43f039f401020e3348850536dfe99c2d6b4b9a49
Amanda <amandaenstad@gmail.com>
2020-10-04 21:42:49 -0500

    Added the showMetadata function

--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,61 @@
 // note: parts of this code are taken from Office Hours on 9/29 and 10/1
 
 function DrawBargraph(sampleId) {
+    d3.json("samples.json").then((data) => {
+
+        var samples = data.samples;
+        var resultArray = samples.filter(x => x.id == sampleId);
+        var result = resultArray[0];
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+        var yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse()
+
+        var barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: {t: 30, l: 150}
+
+        }
+
+        var barData = {
+            x: sample_values.slice(0, 10).reverse(),
+            y: yticks,
+            text: otu_labels.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: "h"
+        }
 
 
+        Plotly.newPlot("bar", [barData], barLayout);
+    });
 }
 
+
+
 function DrawBubblechart(sampleId) {
+    d3.json("samples.json").then((data) => {
 
-
+    });
+    
 }
 
 
 function ShowMetadata(sampleId) {
+    d3.json("samples.json").then((data) => {
+        var metadata = data.metadata;
+        var resultArray = metadata.filter(x => x.id == sampleId);
+        var results = resultArray[0];
+        var panel = d3.select("#sample-metadata");
 
+        console.log(panel);
+        panel.html("");
 
+        Object.entries(results).forEach( ([key, value]) => {
+            var textToShow = `${key}: ${value}`;
+            panel.append("h6").text(textToShow);
+
+        });
+    });
 }
 
 
@@ -49,7 +91,6 @@ function initDashboard() {
 
 
     });
-
 }
 
 initDashboard();
